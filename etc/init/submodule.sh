96c6e3e62e431d8cb90d7bdcabd8085d74f4dd08
Onishi Takeshi <march.hare.come@gmail.com>
2017-07-28 03:39:49 +0900

    initではsubmodule initするのみ

--- etc/init/submodule.sh
+++ etc/init/submodule.sh
@@ -1,4 +1,4 @@
-git submodule add https://github.com/VundleVim/Vundle.vim.git ~/.vim/vundle.git
+git submodule update --init --recursive
 
 # BundleUpdate
 # From: https://github.com/VundleVim/Vundle.vim/wiki/Tips-and-Tricks
