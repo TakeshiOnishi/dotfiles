816ec777ff3de20c481ebf3d98d46713d0fc8699
Onishi Takeshi <march.hare.come@gmail.com>
2017-08-26 22:06:02 +0900

    bin/配下のパスを通す&imgcat導入

---
+++ etc/init/imgcat.sh
@@ -0,0 +1,6 @@
+# imgcatをlocal用binへコピーする
+
+cd ~/dotfiles/bin/_local/
+
+wget -O imgcat https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat
+chmod +x imgcat
---
+++ etc/init/path.sh
@@ -0,0 +1,5 @@
+# bin用PATH追加
+export PATH=$PATH:~/dotfiles/bin/
+
+# bin/_local/用パス追加
+export PATH=$PATH:~/dotfiles/bin/_local/
