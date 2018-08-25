1644a701cded4018dc3d4459dc2f3402de1209e0
Onishi Takeshi <march.hare.come@gmail.com>
2018-08-25 20:35:14 +0900

    Finderメモリクリア後の新しいウィンドウパスを/Users/onishi_takeshiに変更

--- .zsh/rc/mac_cache_clear.zsh
+++ .zsh/rc/mac_cache_clear.zsh
@@ -4,9 +4,13 @@ function mcc(){
 
   cd ~/Library/Preferences
   mv com.apple.finder.plist com.apple.finder.plist_OLD
-  killall Finder
   qlmanage -r cache
   qlmanage -r
+  killall Finder
+  sleep 8s # plist生成までwait
+  plutil -replace 'NewWindowTarget' -string 'PfDe' com.apple.finder.plist # 新規ウィンドウの設定(1/2)
+  plutil -replace 'NewWindowTargetPath' -string "file://${HOME}/" com.apple.finder.plist # 新規ウィンドウの設定(2/2)
+  killall Finder
 
   cd $NOW_DIR
   echo "### MacCacheClear [END] ###"
