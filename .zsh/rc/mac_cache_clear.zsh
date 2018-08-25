73a93139903d66be3fbd7d159c27e3b4508d8146
Onishi Takeshi <march.hare.come@gmail.com>
2018-08-25 22:08:09 +0900

    手動でのプロパティチェックをdefaultsコマンドを利用しての書き込みに大幅変更

--- .zsh/rc/mac_cache_clear.zsh
+++ .zsh/rc/mac_cache_clear.zsh
@@ -1,4 +1,7 @@
+# MacCacheClear (for Finder)
 function mcc(){
+  # もはやCacheクリアというより、設定ファイル再配置になったのでいつか分解する
+  
   echo "### MacCacheClear [START] ###"
   NOW_DIR=$(cd $(dirname $0); pwd)
 
@@ -9,45 +12,14 @@ function mcc(){
   qlmanage -r
   sudo killall cfprefsd #plistCacheReset
   sleep 5s #生成までやや待ち
-  setFinderOption
-  sudo killall cfprefsd #plistCacheReset
+  defaults write com.apple.finder NewWindowTarget -string "PfLo"
+  defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
+  defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+  defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+  defaults write com.apple.finder ShowRecentTags -bool false
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true #Finderではないが追加 (.DSファイルを書き込まない)
   killall Finder
 
   cd $NOW_DIR
   echo "### MacCacheClear [END] ###"
-}
-
-# Finderのプロパティを上書きする関数
-function setFinderOption(){
-  # 上書きしたいプロパティが存在しているかチェック
-  nwt_exist=$(plutil -extract 'NewWindowTarget' xml1  -o - com.apple.finder.plist | sed -n "s/.*<string>\(.*\)<\/string>.*/\1/p")
-  nwtp_exist=$(plutil -extract 'NewWindowTargetPath' xml1  -o - com.apple.finder.plist | sed -n "s/.*<string>\(.*\)<\/string>.*/\1/p")
-  fpvs_exist=$(plutil -extract 'FXPreferredViewStyle' xml1  -o - com.apple.finder.plist | sed -n "s/.*<string>\(.*\)<\/string>.*/\1/p")
-
- # 新規ウィンドウの設定(1/2)
- if [ -z "$nwt_exist" ]; then
-   plutil -insert 'NewWindowTarget' -string 'PfDe' com.apple.finder.plist
-   echo "insert"
- else
-   plutil -replace 'NewWindowTarget' -string 'PfDe' com.apple.finder.plist
-   echo "repalce"
- fi
-
- # 新規ウィンドウの設定(2/2)
- if [ -z "$nwtp_exist" ]; then
-   plutil -insert 'NewWindowTargetPath' -string "file://${HOME}/" com.apple.finder.plist
-   echo "insert"
- else
-   plutil -replace 'NewWindowTargetPath' -string "file://${HOME}/" com.apple.finder.plist
-   echo "repalce"
- fi
-
- # ウィンドウのスタイルをリスト表示にする
- if [ -z "$fpvs_exist" ]; then
-   plutil -insert 'FXPreferredViewStyle' -string 'Nlsv' com.apple.finder.plist
-   echo "insert"
- else
-   plutil -replace 'FXPreferredViewStyle' -string 'Nlsv' com.apple.finder.plist
-   echo "repalce"
- fi
 }
