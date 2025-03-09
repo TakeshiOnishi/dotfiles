74315f225a78eaeea1220a220d59ccc269dafed5
Onishi Takeshi <march.hare.come@gmail.com>
2025-03-09 22:27:01 +0900

    hlslensのhighlightをBasicSetへ変更

--- nvim/lua/plugins/hlslens.lua
+++ nvim/lua/plugins/hlslens.lua
@@ -9,7 +9,7 @@ return {
 
         local text, chunks
         text = ('[%d/%d]'):format(idx, #posList)
-        chunks = {{' ', 'Ignore'}, {text, 'VM_Extend'}}
+        chunks = {{' ', 'Ignore'}, {text, 'Comment'}}
         render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
       end
     }
