7fca2fefc72f3c9f6aa47f163e76b04fb0bd8640
Onishi Takeshi <march.hare.come@gmail.com>
2025-03-09 20:00:30 +0900

    nerd-tree操作に統一

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,38 +1,52 @@
 return {
-    {
-      "nvim-tree/nvim-tree.lua",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      config = function()
-        local api = require("nvim-tree.api")
-  
-        -- nvim-tree のキーマッピングを設定
-        local function on_attach(bufnr)
-          local function opts(desc)
-            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-          end
-  
-          api.config.mappings.default_on_attach(bufnr)
-          vim.keymap.set("n", "<C-e>", api.tree.toggle, opts("Toggle NvimTree"))
-          vim.keymap.set("n", "L", "Lzz", opts("Move to Bottom of View"))
-          vim.keymap.set("n", "H", "Hzz", opts("Move to Top of View"))
-         end
-  
-        require("nvim-tree").setup({
-          on_attach = on_attach, -- 正しくマッピングを適用
-          view = {
-            width = 45, -- デフォルト幅
-          },
-          filters = {
-            dotfiles = false, -- 隠しファイルを表示
-            custom = {}, -- 特定のファイルを非表示にしない
-          },
-          git = {
-            ignore = false, -- `.gitignore` の影響を受けない
-          },
-        })
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local api = require("nvim-tree.api")
 
-        vim.keymap.set("n", "<C-e>", api.tree.toggle, { noremap = true, silent = true, desc = "Toggle NvimTree" })
+      -- 旧個人設定
+      local function on_attach(bufnr)
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        api.config.mappings.default_on_attach(bufnr)
+        vim.keymap.set("n", "<C-e>", api.tree.toggle, opts("Toggle NvimTree"))
+        vim.keymap.set("n", "C", api.tree.change_root_to_node, opts("CD"))
+        vim.keymap.set("n", "u", api.tree.change_root_to_parent, opts("Up"))
+        vim.keymap.set("n", "ma", api.fs.create, opts("Create File Or Directory"))
+        vim.keymap.set("n", "mm", api.fs.rename_full, opts("Rename: Full Path"))
+        vim.keymap.set("n", "md", api.fs.remove, opts("Delete"))
+        vim.keymap.set("n", "L", "Lzz", opts("Move to Bottom of View"))
+        vim.keymap.set("n", "H", "Hzz", opts("Move to Top of View"))
       end
-    }
+
+      opts = {
+        on_attach = on_attach,
+        view = {
+          width = 45,
+        },
+        filters = {
+          dotfiles = false,
+          custom = {},
+        },
+        git = {
+          ignore = false,
+        },
+        actions = {
+          open_file =  {
+            window_picker = {
+              enable = false,
+            }
+          },
+        }
+      }
+
+      require("nvim-tree").setup(opts)
+
+      vim.keymap.set("n", "<C-e>", api.tree.toggle, { noremap = true, silent = true, desc = "Toggle NvimTree" })
+    end
   }
-  
+}
+
